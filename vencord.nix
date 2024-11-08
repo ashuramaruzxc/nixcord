f62c50280d157ab0df2c2bb3847c70cf73975b82
tenjin <ashuramaru@tenjin-dk.com>
2024-11-08 11:12:12 +0100

    vencord1.10.5 -> nixpkgs.vencord1.10.5

--- vencord.nix
+++ vencord.nix
@@ -30,7 +30,7 @@ stdenv.mkDerivation (finalAttrs: {
   pnpmDeps = pnpm.fetchDeps {
     inherit (finalAttrs) pname src;
 
-    hash = "sha256-YBWe4MEmFu8cksOIxuTK0deO7q0QuqgOUc9WkUNBwp0=";
+    hash = "sha256-bosCE9gBFCcM3Ww6sJmhps/cl4lovXKMieYpkqAMst8=";
   };
 
   nativeBuildInputs = [
